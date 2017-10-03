b868066991a7165dd0cd3fa340926e92be9e0a31
Arnaud Dorgans <ineox@me.com>
2017-10-03 17:06:06 +0200

    fix template

--- xctemplates/ViewModel.xctemplate/___FILEBASENAME___.swift
+++ xctemplates/ViewModel.xctemplate/___FILEBASENAME___.swift
@@ -5,22 +5,23 @@
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //___COPYRIGHT___
 //
+
 import Foundation
 import IOViewModel
 
-class ___FILEBASENAMEASIDENTIFIER___ViewModel: IOViewModel {
-
+class ___FILEBASENAMEASIDENTIFIER___: IOViewModel {
+    
     typealias In = Input
     typealias Out = Output
     
     private (set) lazy var `in`: In = In(vm: self)
     private (set) lazy var out: Out = Out(vm: self)
     
-    class Input: ViewModelIn<TestViewModel> {
+    class Input: IOViewModelIn<___FILEBASENAMEASIDENTIFIER___> {
         
     }
     
-    class Output: ViewModelOut<TestViewModel> {
+    class Output: IOViewModelOut<___FILEBASENAMEASIDENTIFIER___> {
         
     }
 }
