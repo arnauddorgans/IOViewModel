56a6525a137f233d4c0fda938fb243ac7f0a3f4b
Arnaud Dorgans <ineox@me.com>
2018-03-11 19:59:55 +0100

    fix

--- xctemplates/ViewModel.xctemplate/___FILEBASENAME___.swift
+++ xctemplates/ViewModel.xctemplate/___FILEBASENAME___.swift
@@ -11,11 +11,8 @@ import IOViewModel
 
 class ___FILEBASENAMEASIDENTIFIER___: IOViewModel {
     
-    typealias In = Input
-    typealias Out = Output
-    
-    private (set) lazy var `in`: In = In(vm: self)
-    private (set) lazy var out: Out = Out(vm: self)
+    private (set) lazy var `in` = Input(vm: self)
+    private (set) lazy var out = Output(vm: self)
     
     class Input: IOViewModelIn<___FILEBASENAMEASIDENTIFIER___> {
         
